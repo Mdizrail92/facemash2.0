abfeafc5cb3740a457aefce70734e4f90f77ded8
Israil Alam <52991105+Mdizrail92@users.noreply.github.com>
2021-05-06 22:53:59 +0530

    Marvel vs DC
    Added more characters, vedios, sign up and more

--- js/script.js
+++ js/script.js
@@ -17,3 +17,130 @@ window.onclick = function(event) {
 }
 
 } );
+
+
+//firebase linking ang getting data
+var firebaseConfig = {
+  apiKey: "AIzaSyA9CUYNIIGaFuMbUadP6Y8uqozt4kJ8NN4",
+  authDomain: "facemash-b52c5.firebaseapp.com",
+  projectId: "facemash-b52c5",
+  storageBucket: "facemash-b52c5.appspot.com",
+  messagingSenderId: "959048373598",
+  appId: "1:959048373598:web:c26e543c2d227921062bcf",
+  measurementId: "G-FQGF02RLF2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+  //Reference messages collection
+  var messagesRef = firebase.database().ref('visiters');
+
+//listen for form submit
+document.getElementById('visiters').addEventListener('submit', submitForm);
+
+//Submit form
+function submitForm(e){
+    e.preventDefault();
+
+    //get Value
+    var name = document.getElementById('name').value ;
+    var instaID = document.getElementById('insta-id').value ;
+
+    //save message
+    saveMessage(name, instaID);
+
+ //Clear form
+ document.getElementById('visiters').reset();
+ window.location.href = 'index1.html';
+    
+}
+
+
+//function to get form values
+function getInput(id){
+  return document.getElementById(id).value;
+}
+
+//Save message to firebase
+function saveMessage(name, instaID){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+      name: name,
+     instaID:instaID
+
+  });
+}
+
+
+// var marvel_clicks = 0;
+// var dc_clicks =0;
+// function marvel_counter() {
+//   marvel_clicks += 1;  
+// }
+// function dc_counter() {
+//   dc_clicks += 1;  
+// }
+// function result()
+// {
+//   marvel_clicks  +=1;
+//   if(marvel_clicks>=4)
+//   {
+   
+//       if (typeof(Storage) !== "undefined") {
+//         if (localStorage.clickcount) {
+//           localStorage.clickcount = Number(localStorage.clickcount)+1;
+//         } else {
+//           localStorage.clickcount = 1;
+//         }
+//         document.getElementById("marvel_result").innerHTML =   "You Are one of the "  + localStorage.clickcount + " <img src=../images/marvel-logo-34301.png> fans vsisited this site till now.";
+//       } else {
+//         document.getElementById("marvel_result").innerHTML = "Sorry, your browser does not support web storage...";
+//       }
+//       var x=document.getElementById("marvel-fan");
+//       x.style.display="block";
+//       var y=document.getElementById("myCarousel");
+//       y.style.display="none";
+    
+//   }
+//   else
+//   {
+//     dc_clicks +=1;
+//     if (typeof(Storage) !== "undefined") {
+//       if (localStorage.clickcountdc) {
+//         localStorage.clickcountdc = Number(localStorage.clickcountdc)+1;
+//       } else {
+//         localStorage.clickcountdc = 1;
+//       }
+//       document.getElementById("dc_result").innerHTML =   "You Are one of the "  + localStorage.clickcountdc + " <img src=../images/dc-logo.png> fans vsisited this site till now.";
+//     } else {
+//       document.getElementById("dc_result").innerHTML = "Sorry, your browser does not support web storage...";
+//     }
+//     var x=document.getElementById("dc-fan");
+//     x.style.display="block";
+//     var y=document.getElementById("myCarousel");
+//     y.style.display="none";
+//   }
+// }
+
+
+  
+//  function feedback_happy(){
+//    alert("Glad to hear it.");
+//  }
+//  function feedback_sad(){
+//   alert("Hope it will be great next time.");
+// }
+
+// function result_marvel(){
+//   var x=document.getElementById("marvel-fan");
+//   x.style.display="block";
+//   var y=document.getElementById("myCarousel");
+//   y.style.display="none";
+// }
+// function result_dc(){
+//   var x=document.getElementById("dc-fan");
+//   x.style.display="block";
+//   var y=document.getElementById("myCarousel");
+//   y.style.display="none";
+// }
--- js/script1.js
+++ js/script1.js
@@ -1,11 +1,80 @@
-$(document).ready(function () {
+
+ $('.carousel').carousel({
+    interval: 1000 * 50
+     });
+ 
+ var marvel_clicks = 0;
+ var dc_clicks =0;
+ function marvel_counter() {
+   marvel_clicks += 1;  
+   console.log(marvel_clicks);
+ }
+ function dc_counter() {
+   dc_clicks += 1;  
+ }
+ function result()
+ {
+   marvel_clicks  +=1;
+  
+   if(marvel_clicks>=4 || marvel_clicks > dc_clicks+1)
+   {
+    
+       if (typeof(Storage) !== "undefined") {
+         if (localStorage.clickcount) {
+           localStorage.clickcount = Number(localStorage.clickcount)+1;
+         } else {
+           localStorage.clickcount = 1;
+         }
+         document.getElementById("marvel_result").innerHTML =   "You Are one of the "  + localStorage.clickcount + " <img src=../images/marvel-logo-34301.png> fans vsisited this site till now.";
+       } else {
+         document.getElementById("marvel_result").innerHTML = "Sorry, your browser does not support web storage...";
+       }
+       var x=document.getElementById("marvel-fan");
+       x.style.display="block";
+       var y=document.getElementById("myCarousel");
+       y.style.display="none";
      
-    //image height
-    var winHeight = $(window).height();
-    var winHeightImg = $(window).height() - 140;
-    $('container').css('height', winHeightImg);
-    $('container').css('height', winHeight);
+   }
+   else
+   {
+     dc_clicks +=1;
+     if (typeof(Storage) !== "undefined") {
+       if (localStorage.clickcountdc) {
+         localStorage.clickcountdc = Number(localStorage.clickcountdc)+1;
+       } else {
+         localStorage.clickcountdc = 1;
+       }
+       document.getElementById("dc_result").innerHTML =   "You Are one of the "  + localStorage.clickcountdc + " <img src=../images/dc-logo.png> fans vsisited this site till now.";
+     } else {
+       document.getElementById("dc_result").innerHTML = "Sorry, your browser does not support web storage...";
+     }
+     var x=document.getElementById("dc-fan");
+     x.style.display="block";
+     var y=document.getElementById("myCarousel");
+     y.style.display="none";
+   }
+ }
+ 
+ 
    
-
-})
-
+  function feedback_happy(){
+    alert("Glad to hear it.");
+  }
+  function feedback_sad(){
+   alert("Hope it will be great next time.");
+ }
+ 
+ function result_marvel(){
+   var x=document.getElementById("marvel-fan");
+   x.style.display="block";
+   var y=document.getElementById("myCarousel");
+   y.style.display="none";
+ }
+ function result_dc(){
+   var x=document.getElementById("dc-fan");
+   x.style.display="block";
+   var y=document.getElementById("myCarousel");
+   y.style.display="none";
+ }
+ 
+ 
